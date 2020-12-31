fad3aadd07b299b87a2b72c82c722a30b3c996d9
s1963887 <s1963887@online.houstonisd.org>
2020-12-31 10:55:41 -0600

    code written

---
+++ Bob.js
@@ -0,0 +1,28 @@
+class Bob {
+    constructor(x, y, r) {
+      var options = {
+          isStatic:true,
+          restitution:0.3,
+          friction:0.5,
+          density:1.2
+      }
+      this.x = x;
+      this.y = y;
+      this.r = r;
+      this.body = Bodies.circle(x, y, r, options);
+      
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      push();
+      translate(pos.x, pos.y);
+      rectMode(CENTER);
+      fill("purple");
+    ellipse(0,0,this.r,this.r);
+      pop();
+    }
+  }
+
+  
